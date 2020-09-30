98a1db5c02c50544b81d8b0e4a6dd21bb5a8a6cd
piotr-sawicki <sawickipiotr.92@gmail.com>
2020-09-30 21:08:09 +0200

    Task "Acceptable Password I" solved!

---
+++ Elementary/Acceptable_password_i.py
@@ -0,0 +1,15 @@
+# https://py.checkio.org/en/mission/acceptable-password-i/
+
+def is_acceptable_password(password: str) -> bool:
+    return True if len(password) > 6 else False
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(is_acceptable_password('short'))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert is_acceptable_password('short') == False
+    assert is_acceptable_password('muchlonger') == True
+    assert is_acceptable_password('ashort') == False
+    print("Coding complete? Click 'Check' to earn cool rewards!")
