fde361a2e6589084240f0464133e40542c12b067
piotr-sawicki <sawickipiotr.92@gmail.com>
2020-10-06 21:06:35 +0200

    Task "Sum Numbers" solved!

---
+++ Home/Sum_numbers.py
@@ -0,0 +1,26 @@
+# https://py.checkio.org/en/mission/sum-numbers/
+
+def sum_numbers(text: str) -> int:
+    sum_i = 0
+    for i in text.split(' '):
+        try:
+            sum_i += int(i)
+        except ValueError:
+            pass
+    return sum_i
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(sum_numbers('hi'))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert sum_numbers('hi') == 0
+    assert sum_numbers('who is 1st here') == 0
+    assert sum_numbers('my numbers is 2') == 2
+    assert sum_numbers('This picture is an oil on canvas '
+ 'painting by Danish artist Anna '
+ 'Petersen between 1845 and 1910 year') == 3755
+    assert sum_numbers('5 plus 6 is') == 11
+    assert sum_numbers('') == 0
+    print("Coding complete? Click 'Check' to earn cool rewards!")
