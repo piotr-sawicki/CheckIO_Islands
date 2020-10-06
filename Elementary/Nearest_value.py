2975170062b71e9e1cceb64f82dd8cdec2b2cccb
piotr-sawicki <sawickipiotr.92@gmail.com>
2020-10-06 20:30:50 +0200

    Task "Nearest Value" solved!

---
+++ Elementary/Nearest_value.py
@@ -0,0 +1,23 @@
+# https://py.checkio.org/en/mission/nearest-value/
+
+def nearest_value(values: set, one: int) -> int:
+    values = sorted(list(values))
+    dist_list = [abs(i - one) for i in values]
+    dist_index = dist_list.index(min(dist_list))
+    return values[dist_index]
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(nearest_value({4, 7, 10, 11, 12, 17}, 9))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 9) == 10
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 8) == 7
+    assert nearest_value({4, 8, 10, 11, 12, 17}, 9) == 8
+    assert nearest_value({4, 9, 10, 11, 12, 17}, 9) == 9
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 0) == 4
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 100) == 17
+    assert nearest_value({5, 10, 8, 12, 89, 100}, 7) == 8
+    assert nearest_value({-1, 2, 3}, 0) == -1
+    print("Coding complete? Click 'Check' to earn cool rewards!")
