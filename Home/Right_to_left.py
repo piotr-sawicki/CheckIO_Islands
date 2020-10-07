aa2afe4d42c7e124bf136a227b1de98a1c036623
piotr-sawicki <sawickipiotr.92@gmail.com>
2020-10-07 18:55:47 +0200

    Task "Right To Left" solved!

---
+++ Home/Right_to_left.py
@@ -0,0 +1,23 @@
+# https://py.checkio.org/en/mission/right-to-left/
+
+
+def left_join(phrases: tuple) -> str:
+    """
+        Join strings and replace "right" to "left"
+    """
+    string = ''
+    for text in phrases:
+        string += text.replace('right', 'left') + ','
+    return string[:-1]
+
+
+if __name__ == '__main__':
+    print('Example:')
+    print(left_join(("left", "right", "left", "stop")))
+
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert left_join(("left", "right", "left", "stop")) == "left,left,left,stop", "All to left"
+    assert left_join(("bright aright", "ok")) == "bleft aleft,ok", "Bright Left"
+    assert left_join(("brightness wright",)) == "bleftness wleft", "One phrase"
+    assert left_join(("enough", "jokes")) == "enough,jokes", "Nothing to replace"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
