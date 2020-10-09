0cc0966e90776ceafffc88e34f22c6493d1f4413
piotr-sawicki <sawickipiotr.92@gmail.com>
2020-10-09 17:44:27 +0200

    Task "Split List" solved!

---
+++ Home/Split_list.py
@@ -0,0 +1,21 @@
+# https://py.checkio.org/en/mission/split-list/
+
+from math import ceil
+
+
+def split_list(items: list) -> list:
+    index_to_slice = ceil(len(items) / 2)
+    return [items[:index_to_slice], items[index_to_slice:]]
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(split_list([1, 2, 3, 4, 5, 6]))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert split_list([1, 2, 3, 4, 5, 6]) == [[1, 2, 3], [4, 5, 6]]
+    assert split_list([1, 2, 3]) == [[1, 2], [3]]
+    assert split_list([1, 2, 3, 4, 5]) == [[1, 2, 3], [4, 5]]
+    assert split_list([1]) == [[1], []]
+    assert split_list([]) == [[], []]
+    print("Coding complete? Click 'Check' to earn cool rewards!")
