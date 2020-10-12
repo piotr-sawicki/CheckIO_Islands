22dd06fb583e720332d734e2a29528320c5b5b55
piotr-sawicki <sawickipiotr.92@gmail.com>
2020-10-12 10:45:58 +0200

    Task "Goes After" solved!

---
+++ Scientific Expedition/Goes_after.py
@@ -0,0 +1,23 @@
+# https://py.checkio.org/en/mission/goes-after/
+
+def goes_after(word: str, first: str, second: str) -> bool:
+    try:
+        return word.index(first) + 1 == word.index(second)
+    except ValueError:
+        return False
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(goes_after('world', 'w', 'o'))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert goes_after('world', 'w', 'o') == True
+    assert goes_after('world', 'w', 'r') == False
+    assert goes_after('world', 'l', 'o') == False
+    assert goes_after('panorama', 'a', 'n') == True
+    assert goes_after('list', 'l', 'o') == False
+    assert goes_after('', 'l', 'o') == False
+    assert goes_after('list', 'l', 'l') == False
+    assert goes_after('world', 'd', 'w') == False
+    print("Coding complete? Click 'Check' to earn cool rewards!")
